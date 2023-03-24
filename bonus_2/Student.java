d996ca09f2974f6ba9667294e21364d344aa869c
Giovanni <giodamico98@gmail.com>
2023-03-24 18:21:53 +0100

    Ex b2 uploaded

--- bonus_2/Student.java
+++ bonus_2/Student.java
@@ -1,4 +1,33 @@
 package javaoop.exercises._2;
 
 public class Student {
-}
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        System.out.println("Constructing...");
+        this.name = name;
+        this.age = age;
+        System.out.println("...Constructed!");
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+         //   a Constructor that takes only a student's age
+    public Student(Integer age) {
+        this.age = age;
+        System.out.println("Student age: " + age);
+    }
+  //  a Constructor that takes only a student's name
+    public Student(String name) {
+        this.name = name;
+        System.out.println("Student name: " + name);
+    }
+    //a Constructor that does not take any parameters
+    public Student() {
+    }
+ }
+
